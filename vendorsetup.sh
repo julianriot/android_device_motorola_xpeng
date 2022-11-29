5618924c84efb3d3dd3fa4ac677263f7a86db897
julianriot <58826052+julianriot@users.noreply.github.com>
2022-11-29 16:47:31 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_xpeng-user
-add_lunch_combo omni_xpeng-userdebug
-add_lunch_combo omni_xpeng-eng
+add_lunch_combo twrp_xpeng-user
+add_lunch_combo twrp_xpeng-userdebug
+add_lunch_combo twrp_xpeng-eng
